702bb4a678b828c7787f3dd17cbbba4bce8b20ea
Richard Hao <richard@digitalstar.tech>
2023-01-04 23:01:08 +0800

    feat: add upvote functionality to forum contract

--- Hardhat/contracts/Forum.sol
+++ Hardhat/contracts/Forum.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract Forum {
     struct Question {
         uint questionId;
@@ -21,9 +23,27 @@ contract Forum {
     Question[] public questions;
     Answer[] public answers;
     mapping(uint => uint[]) public answerPerQuestion;
+    mapping(uint => mapping(address => bool)) public upvoters;
+    mapping(address => uint) public userUpvoteCount;
 
     event QuestionAdded(Question question);
     event AnswerAdded(Answer answer);
+    event AnswerUpvoted(Answer answer);
+
+    IERC20 public immutable Goflow;
+    uint constant decimal = 18;
+
+    uint amountToPay = 1 * 10 ** decimal;
+    uint amountToPayParticipate = 10 * 10 ** decimal;
+
+    constructor(address _tokenAddress) {
+        Goflow = IERC20(_tokenAddress);
+    }
+
+    modifier answerExists(uint _answerId) {
+        require(answers.length >= _answerId, "Answer does not exist");
+        _;
+    }
 
     function postQuestion(string calldata _message) external {
         uint questionCounter = questions.length;
@@ -52,11 +72,60 @@ contract Forum {
         emit AnswerAdded(answer);
     }
 
+    function upvoteAnswer(uint _answerId) external answerExists(_answerId) {
+        Answer storage currentAnswer = answers[_answerId];
+
+        require(
+            upvoters[_answerId][msg.sender] != true,
+            "You already upvoted this answer"
+        );
+        require(
+            currentAnswer.creatorAddress != msg.sender,
+            "You cannot upvote your own answer"
+        );
+        require(
+            Goflow.balanceOf(msg.sender) >= amountToPay,
+            "You do not have enough GOFLOW tokens to upvote this answer"
+        );
+        require(
+            Goflow.allowance(msg.sender, address(this)) >= amountToPay,
+            "You need to approve the contract to spend your GOFLOW tokens"
+        );
+
+        bool sent;
+        if (
+            Goflow.balanceOf(currentAnswer.creatorAddress) >=
+            amountToPayParticipate
+        ) {
+            sent = Goflow.transferFrom(
+                msg.sender,
+                currentAnswer.creatorAddress,
+                amountToPay
+            );
+        } else {
+            sent = Goflow.transferFrom(msg.sender, address(this), amountToPay);
+        }
+
+        require(sent, "Failed to send GOFLOW tokens");
+        currentAnswer.upvotes++;
+        userUpvoteCount[msg.sender]++;
+        upvoters[_answerId][msg.sender] = true;
+        emit AnswerUpvoted(currentAnswer);
+    }
+
+    function getUpvotes(
+        uint _answerId
+    ) public view answerExists(_answerId) returns (uint) {
+        return answers[_answerId].upvotes;
+    }
+
     function getQuestions() external view returns (Question[] memory) {
         return questions;
     }
 
-    function getAnswersPerQuestion(uint _questionId) external view returns (uint[] memory) {
+    function getAnswersPerQuestion(
+        uint _questionId
+    ) public view returns (uint[] memory) {
         return answerPerQuestion[_questionId];
     }
 }
--- Hardhat/test/OurToken.test.ts
+++ Hardhat/test/OurToken.test.ts
@@ -1,81 +1,87 @@
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
-import { expect } from "chai"
-import { Contract } from "ethers"
-import { ethers } from "hardhat"
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from "chai";
+import { Contract } from "ethers";
+import { ethers } from "hardhat";
 
-describe("GoFlow", function() {
-  let goflow: Contract, owner: SignerWithAddress, otherAccount: SignerWithAddress
+describe("GoFlow", function () {
+  let goflow: Contract,
+    owner: SignerWithAddress,
+    otherAccount: SignerWithAddress;
 
   const deployContract = async () => {
-    const [_owner, _otherAccount] = await ethers.getSigners()
-    const Goflow = await ethers.getContractFactory("Goflow")
-    goflow = await Goflow.deploy()
-    owner = _owner
-    otherAccount = _otherAccount
-  }
+    const [_owner, _otherAccount] = await ethers.getSigners();
+    const Goflow = await ethers.getContractFactory("Goflow");
+    goflow = await Goflow.deploy();
+    owner = _owner;
+    otherAccount = _otherAccount;
+  };
 
   const mint = async (user: SignerWithAddress, amount: number) => {
-    const tx = await goflow.connect(user).mint(amount)
-    await tx.wait()
-  }
+    const tx = await goflow.connect(user).mint(amount);
+    await tx.wait();
+  };
 
   beforeEach(async () => {
-    await deployContract()
-  })
+    await deployContract();
+  });
 
   describe("Deployment", () => {
     it("Should deploy and return correct symbol", async () => {
-      expect(await goflow.symbol()).to.equal("GOFLOW")
-    })
-  })
+      expect(await goflow.symbol()).to.equal("GOFLOW");
+    });
+  });
 
   describe("Minting", () => {
     it("Should mint tokens to user", async () => {
-      await mint(owner, 100)
-      expect(await goflow.balanceOf(owner.address)).to.equal(100)
-    })
-  })
+      await mint(owner, 100);
+      expect(await goflow.balanceOf(owner.address)).to.equal(100);
+    });
+  });
 
   describe("Burning", () => {
     it("Should burn tokens from the user", async () => {
-      await mint(owner, 100)
-      expect(await goflow.balanceOf(owner.address)).to.equal(100)
-      expect(await goflow.balanceOf(otherAccount.address)).to.equal(0)
+      await mint(owner, 100);
+      expect(await goflow.balanceOf(owner.address)).to.equal(100);
+      expect(await goflow.balanceOf(otherAccount.address)).to.equal(0);
 
-      const tx = await goflow.connect(owner).burn(20)
-      await tx.wait()
-      expect(await goflow.balanceOf(owner.address)).to.equal(80)
-      expect(await goflow.balanceOf(otherAccount.address)).to.equal(0)
-    })
-  })
+      const tx = await goflow.connect(owner).burn(20);
+      await tx.wait();
+      expect(await goflow.balanceOf(owner.address)).to.equal(80);
+      expect(await goflow.balanceOf(otherAccount.address)).to.equal(0);
+    });
+  });
 
   describe("Transfer", () => {
     it("Should transfer tokens to otherAccount", async () => {
-      await mint(owner, 100)
-      await mint(otherAccount, 100)
+      await mint(owner, 100);
+      await mint(otherAccount, 100);
 
-      await goflow.connect(owner).transfer(otherAccount.address, 20)
-      expect(await goflow.balanceOf(owner.address)).to.equal(80)
-      expect(await goflow.balanceOf(otherAccount.address)).to.equal(120)
-    })
-  })
+      await goflow.connect(owner).transfer(otherAccount.address, 20);
+      expect(await goflow.balanceOf(owner.address)).to.equal(80);
+      expect(await goflow.balanceOf(otherAccount.address)).to.equal(120);
+    });
+  });
 
   describe("TransferFrom", () => {
     it("Should approve a spender to be able to transfer owner's tokens", async () => {
-      await mint(owner, 20)
+      await mint(owner, 20);
 
-      const approve = await goflow.approve(otherAccount.address, 20)
-      await approve.wait()
-      const transferFrom = await goflow.connect(otherAccount).transferFrom(owner.address, otherAccount.address, 20)
-      await transferFrom.wait()
-      expect(await goflow.balanceOf(owner.address)).to.equal(0)
-      expect(await goflow.balanceOf(otherAccount.address)).to.equal(20)
-    })
+      const approve = await goflow.approve(otherAccount.address, 20);
+      await approve.wait();
+      const transferFrom = await goflow
+        .connect(otherAccount)
+        .transferFrom(owner.address, otherAccount.address, 20);
+      await transferFrom.wait();
+      expect(await goflow.balanceOf(owner.address)).to.equal(0);
+      expect(await goflow.balanceOf(otherAccount.address)).to.equal(20);
+    });
 
     it("Should not allow spender to transfer more tokens than they have", async () => {
-      await mint(owner, 20)
-      expect(await goflow.balanceOf(owner.address)).to.equal(20)
-      expect(await goflow.approve(otherAccount.address, 20)).to.be.revertedWith("Insufficient balance for approval")
-    })
-  })
-})
+      await mint(owner, 20);
+      expect(await goflow.balanceOf(owner.address)).to.equal(20);
+      expect(await goflow.approve(otherAccount.address, 20)).to.be.revertedWith(
+        "Insufficient balance for approval"
+      );
+    });
+  });
+});
