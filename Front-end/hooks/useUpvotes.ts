30a79b59b9234e519517200384cbe71748c874e5
Richard Hao <richard@digitalstar.tech>
2023-01-10 20:54:26 +0800

    feat: add useAnswers and useUpvotes hooks

---
+++ Front-end/hooks/useUpvotes.ts
@@ -0,0 +1,20 @@
+import { BigNumber } from "ethers";
+import { useQuery } from "react-query";
+import useForumContract from "./contracts/useForumContract";
+
+interface UseUpvotesQuery {
+  answerId: BigNumber;
+}
+
+const useUpvotes = ({ answerId }: UseUpvotesQuery) => {
+  const contract = useForumContract();
+  return useQuery(
+    ["upvotes", answerId.toNumber()],
+    async () => {
+      return await contract.getUpvotes(answerId);
+    },
+    { enabled: !!answerId }
+  );
+};
+
+export default useUpvotes;
