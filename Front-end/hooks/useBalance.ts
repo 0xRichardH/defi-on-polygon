d44289094ef8bbeb5eeb783288602cece057c36c
Richard Hao <richard@digitalstar.tech>
2023-01-13 23:40:44 +0800

    feat: add useBalance hook

---
+++ Front-end/hooks/useBalance.ts
@@ -0,0 +1,26 @@
+import { useQuery } from "react-query";
+import { useAccount } from "wagmi";
+import useForumContract from "./contracts/useForumContract";
+import useGoflowContract from "./contracts/useGoflowContract";
+
+const useBalance = () => {
+  const forumContract = useForumContract();
+  const goflowContract = useGoflowContract();
+  const { address: account } = useAccount();
+
+  const contractBalanceQuery = useQuery(["contractBalance"], async () => {
+    return await goflowContract.getBalance(forumContract.contract.address);
+  });
+
+  const userBalanceQuery = useQuery(["userBalance", account], async () => {
+    if (account) {
+      return await goflowContract.getBalance(account);
+    } else {
+      return "0";
+    }
+  });
+
+  return { contractBalanceQuery, userBalanceQuery };
+};
+
+export default useBalance;
