20c607d4f740627acb60f1578a7006b774149ea9
Richard Hao <richard@digitalstar.tech>
2023-03-10 22:56:02 +0800

    feat(hardhat): add script to deploy to polygon mumbai testnet

--- Hardhat/hardhat.config.ts
+++ Hardhat/hardhat.config.ts
@@ -1,11 +1,22 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+import invariant from "tiny-invariant";
+
+const { PLOYGON_TEST_NET_URL, PRIVATE_KEY } = process.env;
+
+invariant(PLOYGON_TEST_NET_URL, "PLOYGON_TEST_NET_URL is not defined");
+invariant(PRIVATE_KEY, "PRIVATE_KEY is not defined");
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    polygon: {
+      url: PLOYGON_TEST_NET_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
