cf2de5ededd0bc969a6bd57af690c091155e7d3b
Richard Hao <richard@digitalstar.tech>
2023-03-11 21:53:06 +0800

    chore: remove unnecessary code

--- Front-end/pages/index.tsx
+++ Front-end/pages/index.tsx
@@ -1,23 +1,9 @@
 import { Box } from "@chakra-ui/react"
-import { BigNumber } from "ethers"
 import type { NextPage } from "next"
 import * as React from "react"
 import Questions from "../components/Questions"
-import useForumContract from "../hooks/contracts/useForumContract"
 
 const App: NextPage = () => {
-  const contract = useForumContract()
-
-  React.useEffect(() => {
-    const fetchQuestion = async () => {
-      console.log(
-        "are we connecting to the contract?",
-        await contract.getQuestion(BigNumber.from(0))
-      )
-    }
-    fetchQuestion()
-  }, [])
-
   return (
     <Box p={8} maxW="600px" minW="320px" m="0 auto">
       <Questions />
